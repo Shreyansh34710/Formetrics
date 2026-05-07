bb2480bca660b7bc95679cf6061af465cdb917ef
Shreyansh34710 <adi29shreyansh@gmail.com>
2026-05-07 13:11:01 +0530

    basic changs like margin and radius

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div class="h-[10vh] bg-gray-700 text-gray-300 flex items-center justify-evenly pr-[60px] pl-[60px] border-1 rounded-b-3xl border-slate-700">
+        <div class="h-[10vh] bg-gray-700 text-gray-300 flex items-center justify-evenly pr-[60px] pl-[60px] border rounded-b-3xl border-slate-700">
             <div className='h-[35px] w-[2vw] bg-white rounded-sm'></div>
             <div className='h-[35px] w-[2vw] bg-white rounded-sm'></div>
             <div className='h-[35px] w-[2vw] bg-white rounded-sm'></div>
