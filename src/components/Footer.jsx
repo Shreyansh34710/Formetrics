6668facdf4aef803605142ea11fbd5262ba5e3c9
Shreyansh34710 <adi29shreyansh@gmail.com>
2026-05-07 10:37:00 +0530

    footer doone for now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Footer() {
     return (
         <div class="h-[8vh] bg-gray-900 text-gray-300 flex items-center justify-evenly pr-[60px] pl-[60px] box-border">
-            <div className='h-[3vh] w-[2vw] bg-white'></div>
-            <div className='h-[3vh] w-[2vw] bg-white'></div>
-            <div className='h-[3vh] w-[2vw] bg-white'></div>
-            <div className='h-[3vh] w-[2vw] bg-white'></div>
+            <div className='h-[3vh] w-[2vw] bg-white rounded-sm'></div>
+            <div className='h-[3vh] w-[2vw] bg-white rounded-sm'></div>
+            <div className='h-[3vh] w-[2vw] bg-white rounded-sm'></div>
+            <div className='h-[3vh] w-[2vw] bg-white rounded-sm'></div>
         </div>
     )
 }
