83280961617db2f2f8d8ba849c80001e9e76f8ee
Shreyansh34710 <adi29shreyansh@gmail.com>
2026-05-07 11:01:24 +0530

    change in center top

--- src/components/Center.jsx
+++ src/components/Center.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Center() {
     return (
-        <div class="bg-slate-50 h-[80vh] flex  justify-center items-center">
-            <p class="text-gray-600">Main Content</p>
+        <div class="bg-slate-50 h-[80vh] flex-row  justify-center items-center pt-[5px] rounded-sm">
+            <div className='bg-slate-100 h-[10%] w-[100%] mb-[10px] p-[25px]  flex flex-row justify-between items-center'>
+                <h1 className='text-2xl'>Hello, User</h1> 
+                <div className='h-[40px] w-[40px] bg-slate-400 rounded-4xl mr-[5px]'></div>
+            </div>
+            <div className='bg-slate-100 h-[70%] w-[100%] mb-[10px] rounded-sm'></div>
+            <div className='bg-slate-100 h-[15%] w-[100%]'></div>
+            
         </div>
     )
 }
